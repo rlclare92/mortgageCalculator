785cc9301869f127f54a5441e5bc443df830e8a8
rlclare92 <r.l.clare@hotmail.com>
2023-08-02 23:20:04 +0100

    Set-up tenure select

--- src/App.js
+++ src/App.js
@@ -7,25 +7,23 @@ import SliderSelect from "./Components/SliderSelect";
 import TenureSelect from "./Components/TenureSelect";
 
 function App() {
-  
   const [data, setData] = useState({
     homeValue: 3000,
     downPayment: 3000 * 0.2,
     loanAmount: 3000 * 0.8,
     loanTerm: 5,
-    interestRate: 5
-  })
+    interestRate: 5,
+  });
 
   return (
     <div className="App">
       <Navbar />
-      <Container maxWidth="xl" sx={{marginTop:4}}>
+      <Container maxWidth="xl" sx={{ marginTop: 4 }}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
+            <SliderSelect data={data} setData={setData} />
 
-            <SliderSelect data={data} setData={setData}/>
-            
-            <TenureSelect />
+            <TenureSelect data={data} setData={setData} />
           </Grid>
           <Grid item xs={12} md={6}>
             <Result />
--- src/Components/TenureSelect.js
+++ src/Components/TenureSelect.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MeunItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-function TenureSelect() {
+const TenureSelect = ({ data, setData }) => {
+  const handleChange = (event) => {
+    setData({ ...data, loanTerm: event.target.value });
+  };
+
   return (
-    <div>TenureSelect</div>
-  )
-}
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label"> Tenure</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={data.loanTerm}
+        label="Tenure"
+        defaultValue={5}
+        onChange={handleChange}
+      >
+        <MeunItem value={5}>5 Years</MeunItem>
+        <MeunItem value={10}>10 Years</MeunItem>
+        <MeunItem value={15}>15 Years</MeunItem>
+        <MeunItem value={20}>20 Years</MeunItem>
+        <MeunItem value={25}>25 Years</MeunItem>
+      </Select>
+    </FormControl>
+  );
+};
 
-export default TenureSelect
+export default TenureSelect;
