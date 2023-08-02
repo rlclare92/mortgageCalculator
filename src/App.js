ecc3652d70806067d4bec91bc6c33d1c13065758
rlclare92 <r.l.clare@hotmail.com>
2023-08-02 23:10:37 +0100

    Setting-up the slider bars.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import SliderSelect from "./Components/SliderSelect";
 import TenureSelect from "./Components/TenureSelect";
 
 function App() {
+  
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5
+  })
 
   return (
     <div className="App">
@@ -14,7 +22,9 @@ function App() {
       <Container maxWidth="xl" sx={{marginTop:4}}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
-            <SliderSelect />
+
+            <SliderSelect data={data} setData={setData}/>
+            
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
