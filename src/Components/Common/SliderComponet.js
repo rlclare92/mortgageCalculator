52b5c51dbbc156570e97b7c2c1ab637189951f97
rlclare92 <r.l.clare@hotmail.com>
2023-08-02 22:16:07 +0100

    slide bar components

---
+++ src/Components/Common/SliderComponet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SliderComponet() {
+  return (
+    <div>SliderComponet</div>
+// label
+// min
+// max
+// defaultValue
+// unit
+// value
+// steps
+// amount
+// onChange
+  )
+}
+
+export default SliderComponet
